12de3e4ecd0f923343f07ae6595928c23075ad09
Ahmet Talha TURKMEN <75725469+AhmetTalhaTURKMEN@users.noreply.github.com>
2021-09-17 18:37:16 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void tersyaz(char x[],int y){
+    if(y>0){
+    printf("%c",x[y-1]);
+    tersyaz(x,y-1);
+    }
+}
+
+int main()
+{
+    char metin[101];
+    printf("Please input the text: ");
+    gets(metin);
+
+    tersyaz(metin,strlen(metin));
+    return 0;
+}
